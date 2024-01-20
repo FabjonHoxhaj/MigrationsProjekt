a5b2de16f6e4762658c1136b6b5f04a054e8bfe8
unknown <fahox1991@gmail.com>
2024-01-20 19:38:44 +0100

    initial commit

---
+++ src/main/java/com/datamigration/MigrationsProjekt/MigrationsProjektApplication.java
@@ -0,0 +1,13 @@
+package com.datamigration.MigrationsProjekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationsProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationsProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datamigration/MigrationsProjekt/MigrationsProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datamigration.MigrationsProjekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationsProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
